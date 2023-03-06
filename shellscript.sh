69e536c2fbdef7034f0fa3d816260bde0ceeaf23
Muhammet Sönmez <99792163+Qappevox@users.noreply.github.com>
2023-03-06 11:40:53 +0300

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -15,10 +15,27 @@ termux-toast -b white -c black "Merhaba,"
 	termux-toast -b white -c black  "kurulum birkaç dakika içerisinde tamamlanacaktır."
 	sleep 2
 	termux-toast -b white -c black  "@qappevox (github)"
-	sleep 10
-    termux-toast -b white -c black "Lütfen birkaç saniye sonra y yazıp enter tuşuna basınız."
+	sleep 7
+ 
 
 pkg update
+    git clone https://github.com/Ignitetch/AdvPhishing
+    git clone https://github.com/kayaroot/anonimsms
+    git clone https://github.com/TheReaper167/Malicious
+    git clone https://github.com/htr-tech/nexphisher
+    git clone https://github.com/ikiganteng/bot-igeh
+    git clone https://github.com/htr-tech/nexphisher
+    git clone https://github.com/th3unkn0n/osi.ig.git
+    git clone https://github.com/cybuly/instaspam.git
+    git clone https://github.com/kayaroot/geritakip
+    git clone https://github.com/maldevel/IPGeoLocation
+    git clone https://github.com/ELVIN4/bomber-AK47
+    git clone https://github.com/kayaroot/TURK
+    git clone https://github.com/techchipnet/CamPhish
+    git clone https://github.com/mbest99/gametool
+    git clone https://github.com/grafov/hulk.git
+    git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+    git clone https://github.com/htr-tech/bash2mp4
 pkg install zip wget -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
@@ -41,23 +58,7 @@ chmod +x ngrok
 	pkg install unstable-repo -y
 	pkg install metasploit -y
 	
-    git clone https://github.com/Ignitetch/AdvPhishing
-    git clone https://github.com/kayaroot/anonimsms
-    git clone https://github.com/TheReaper167/Malicious
-    git clone https://github.com/htr-tech/nexphisher
-    git clone https://github.com/ikiganteng/bot-igeh
-    git clone https://github.com/htr-tech/nexphisher
-    git clone https://github.com/th3unkn0n/osi.ig.git
-    git clone https://github.com/cybuly/instaspam.git
-    git clone https://github.com/kayaroot/geritakip
-    git clone https://github.com/maldevel/IPGeoLocation
-    git clone https://github.com/ELVIN4/bomber-AK47
-    git clone https://github.com/kayaroot/TURK
-    git clone https://github.com/techchipnet/CamPhish
-    git clone https://github.com/mbest99/gametool
-    git clone https://github.com/grafov/hulk.git
-    git clone https://github.com/AngelSecurityTeam/Cam-Hackers
-    git clone https://github.com/htr-tech/bash2mp4
+
 		exit
 
 }
